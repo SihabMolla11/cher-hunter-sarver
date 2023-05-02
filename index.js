41cbc8bd4f8750e269d0fc597ce2a43b51f9313a
SihabMolla11 <sihabuddinmolla11@gamil.com>
2023-05-02 22:20:36 +0600

    create Data

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express()
 const cors = require('cors')
 const port = 5000
 
+const chefs = require('./datas/data.json')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('hello wordl')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs)
 })
 
 app.listen(port, () => {
